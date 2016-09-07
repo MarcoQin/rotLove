6ef37499b763e1af83b91030d21223318fac3a3c
Paul Lewis <paulofmandown@gmail.com>
2016-09-06 19:20:46 -0500

    Made main random by default

--- Demos/main.lua
+++ Demos/main.lua
@@ -1,50 +1,30 @@
 --[[ Uncomment the Demo you'd like to run! ]]--
 
-			-- [[ Event Queue ]]--
-			--require 'event'
-
-			-- [[ Schedulers ]]--
-			--require 'simple'
-			--require 'speed'
-			--require 'action'
-
-			-- [[ Engine ]]--
-			require 'engine'
-
-			-- [[ String Generator ]]--
-		 	--require 'stringGen'
-
-			--[[ Random Number Generator ]]--
-			--require 'rng'   --state get/set
-
-			-- [[ Disply ]]--
-			--require 'display'
-
-            -- [[ TextDisplay ]]--
-            --require 'textDisplay'
-
-			-- [[ Map Generators ]]--
-			--require 'arena'
-			--require 'dividedMaze'
-			--require 'iceyMaze'
-			--require 'ellerMaze'
-			--require 'cellular'
-			--require 'digger'
-			--require 'uniform'
-			--require 'rogue'
-
-			-- [[ Noise Generator ]]--
-			--require 'simplex'
-
-			-- [[ FOV ]]--
-			-- [[ Shadow Casting ]]--
-			--require 'precise'
-			--require 'preciseWithMovingPlayer'
-			--[[ Ray Casting ]]--
-			--require 'bresenham'
-
-            --[[ Path ]]--
-            --require 'dijkstraMap'
-
-            --[[ Color ]]--
-			--require 'lighting'
+local demos={
+    'event',
+    'simple',
+    'speed',
+    'action',
+    -- 'engine', --Busted at the moment, don't run
+    -- 'stringGen', --Busted at the moment, don't run
+    'rng',
+    'display',
+    'textDisplay',
+    'arena',
+    'dividedMaze',
+    'iceyMaze',
+    'ellerMaze',
+    'cellular',
+    'digger',
+    'uniform',
+    'rogue',
+    'simplex',
+    'precise',
+    'preciseWithMovingPlayer',
+    'bresenham',
+    'dijkstraMap',
+    'lighting',
+}
+math.randomseed(os.time())
+require (demos[math.random(1,#demos)])
+--require 'lighting'
